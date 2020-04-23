733c881f937f3b5c0fe1a11e402715908b4bbbe6
Massimiliano <digiacomo.massimiliano.1981@gmail.com>
2020-04-23 10:07:36 +0200

    scroll to animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,47 @@
+// Animation scroll to
+
+function doScrolling(id, duration) {
+  var startingY = window.pageYOffset;
+  var elementY =
+    window.pageYOffset +
+    document.getElementById(id).getBoundingClientRect().top;
+  // If element is close to page's bottom then window will scroll only to some position above the element.
+  var targetY =
+    document.body.scrollHeight - elementY < window.innerHeight
+      ? document.body.scrollHeight - window.innerHeight
+      : elementY;
+  var diff = targetY - startingY;
+  // Easing function: easeInOutCubic
+  // From: https://gist.github.com/gre/1650294
+  var easing = function (t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+  };
+  var start;
+
+  if (!diff) return;
+
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp;
+    // Elapsed miliseconds since start of scrolling.
+    var time = timestamp - start;
+    // Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1);
+    // Apply the easing.
+    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+    percent = easing(percent);
+
+    window.scrollTo(0, startingY + diff * percent);
+
+    // Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step);
+    }
+  });
+}
+
+// Contatore numero device
+
 function animateValue(id, start, end, duration) {
   // assumes integer values for start and end
 
@@ -34,19 +78,50 @@ function animateValue(id, start, end, duration) {
   run();
 }
 
-// Come usare un google spreadsheet come json endpoint:
-// https://www.freecodecamp.org/news/cjn-google-sheets-as-json-endpoint/
-fetch(
-  "https://spreadsheets.google.com/feeds/cells/1kis3LpXyEQkr2MprLVnzD3P_gL_pgsmAwxCw5e25hKc/1/public/full?alt=json"
-)
+/*
+  Recupero numero di device donati
+
+  Come usare un google spreadsheet come json endpoint:
+  https://www.freecodecamp.org/news/cjn-google-sheets-as-json-endpoint/
+*/
+
+let linkToGoogleDocument =
+  "https://spreadsheets.google.com/feeds/cells/1kis3LpXyEQkr2MprLVnzD3P_gL_pgsmAwxCw5e25hKc/1/public/full?alt=json";
+
+fetch(linkToGoogleDocument)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     try {
-      let devicesNumber = data.feed.entry[0].content.$t;
-      animateValue("devices", 0, parseInt(devicesNumber), 1000);
+      // Se la lettura del documento ha successo, letto l'informazione
+      let devicesNumberAsString = data.feed.entry[0].content.$t;
+
+      // Trasformo il dato da stringa a numero
+      let devicesNumber = parseInt(devicesNumberAsString);
+
+      // Faccio partire l'animazione del contatore
+      animateValue("devices", 0, devicesNumber, 1000);
     } catch (e) {
+      // altrimenti stampo l'errore
       console.log(e);
     }
   });
+
+/*
+  Scroll al box delle donazioni
+*/
+
+let donaOraLink = document.querySelector("[href='#donaBox']");
+
+donaOraLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  /* DoScrolling function
+     Parametri:
+     - id dell'elemento verso cui scrollare
+     - durata dell'animazione
+  */
+  doScrolling("donaBox", 1000);
+});
